025d2c06062e2a55bcc394a4f88510ad0b553fed
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2014-06-08 18:38:24 +1200

    Update for latest teapot.

--- teapot.rb
+++ teapot.rb
@@ -3,23 +3,23 @@ teapot_version "1.0.0"
 
 define_target "build-cmake" do |target|
 	target.provides "Build/CMake" do
-		define "cmake.unix-makefiles", Rule do
+		define Rule, "cmake.unix-makefiles" do
 			input :source
 			
 			parameter :build_prefix
 			
 			output :make_file, implicit: true do |arguments|
-				FSO::Files::Paths.new(arguments[:build_prefix].full_path, "Makefile")
+				Path.join(arguments[:build_prefix], "Makefile")
 			end
 			
 			apply do |arguments|
-				fs.mkpath arguments[:build_prefix].full_path
+				fs.mkpath arguments[:build_prefix]
 				
 				run!("cmake", "-G", "Unix Makefiles",
 					"-DCMAKE_INSTALL_PREFIX:PATH=#{environment[:install_prefix]}",
 					"-DCMAKE_PREFIX_PATH=#{environment[:install_prefix]}",
-					arguments[:source].full_path,
-					chdir: arguments[:build_prefix].full_path
+					arguments[:source],
+					chdir: arguments[:build_prefix]
 				)
 			end
 		end
